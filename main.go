16e49083836d371c2a66f4f65090367c0f78c86c
xerdin442 <mudianthonio27@gmail.com>
2026-01-19 21:17:58 +0100

    added logic to handlie incoming messages

--- main.go
+++ main.go
@@ -1,15 +1,66 @@
 package main
 
-import "golang.org/x/net/websocket"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"golang.org/x/net/websocket"
+)
+
+type Connections struct {
+	m     sync.Mutex
+	store map[*websocket.Conn]bool
+}
 
 type Server struct {
-	conns map[websocket.Conn]bool
+	conns Connections
 }
 
 func NewServer() *Server {
 	return &Server{
-		conns: make(map[websocket.Conn]bool),
+		conns: Connections{
+			store: make(map[*websocket.Conn]bool),
+		},
 	}
 }
 
-func main() {}
+func (s *Server) handleConn(ws *websocket.Conn) {
+	s.conns.m.Lock()
+	defer s.conns.m.Unlock()
+
+	fmt.Println("New incoming connection:", ws.RemoteAddr())
+
+	s.conns.store[ws] = true
+
+	go s.receiveMsg(ws)
+}
+
+func (s *Server) receiveMsg(ws *websocket.Conn) {
+	scanner := bufio.NewScanner(ws)
+
+	for scanner.Scan() {
+		msg := scanner.Text()
+		fmt.Printf("Received message from %v: %s", ws.RemoteAddr(), msg)
+
+		ws.Write([]byte("Thank you!"))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading from connection:", err)
+		return
+	}
+}
+
+func main() {
+	server := NewServer()
+
+	http.Handle("/ws", websocket.Handler(server.handleConn))
+
+	fmt.Println("Server starting on port 8080...")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Server connection error:", err)
+	}
+}
