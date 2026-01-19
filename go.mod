bc1309fdeee6e440d5d93a9cf38bd7afb43e13c1
xerdin442 <mudianthonio27@gmail.com>
2026-01-19 19:05:34 +0100

    installed dependencies

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xerdin442/websocket-feed
+
+go 1.25.4
+
+require golang.org/x/net v0.49.0 // indirect
--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
+import "golang.org/x/net/websocket"
+
+type Server struct {
+	conns map[websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[websocket.Conn]bool),
+	}
+}
+
 func main() {}
